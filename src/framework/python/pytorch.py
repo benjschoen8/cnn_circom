55d0575e4d6960c526aafb5d4ddb256f2e21d24e
benjschoen8 <benjschoen8@gmail.com>
2025-06-28 22:05:58 +0800

    refactor: pytorch

--- src/framework/python/pytorch.py
+++ src/framework/python/pytorch.py
@@ -1,39 +1,60 @@
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
-from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
 import json
 
-def train(epochs=2, batch_size=64, lr=0.01):
+class SimpleCNN(nn.Module):
+    def __init__(self):
+        super(SimpleCNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 8, kernel_size=3, padding=1)
+        self.relu1 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(2)
+
+        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, padding=1)
+        self.relu2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(2)
+
+        self.flatten = nn.Flatten()
+        self.fc = nn.Linear(16 * 7 * 7, 10)
+
+    def forward(self, x):
+        x = self.relu1(self.conv1(x))
+        x = self.pool1(x)
+        x = self.relu2(self.conv2(x))
+        x = self.pool2(x)
+        x = self.flatten(x)
+        x = self.fc(x)
+        return x
+
+def train(epochs=1, batch_size=64, lr=0.01):
     transform = transforms.ToTensor()
     train_dataset = datasets.MNIST(root="data", train=True, download=True, transform=transform)
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
-    conv_w = torch.randn(8, 1, 3, 3, requires_grad=True)
-    conv_b = torch.zeros(8, requires_grad=True)
-    fc_w = torch.randn(10, 8 * 26 * 26, requires_grad=True)
-    fc_b = torch.zeros(10, requires_grad=True)
-
-    optimizer = optim.SGD([conv_w, conv_b, fc_w, fc_b], lr=lr)
+    model = SimpleCNN()
+    optimizer = torch.optim.SGD(model.parameters(), lr=lr)
 
     for _ in range(epochs):
         for data, target in train_loader:
             optimizer.zero_grad()
-            x = F.conv2d(data, conv_w, conv_b)
-            x = F.relu(x)
-            x = torch.flatten(x, 1)
-            x = F.linear(x, fc_w, fc_b)
-            loss = F.cross_entropy(x, target)
+            output = model(data)
+            loss = F.cross_entropy(output, target)
             loss.backward()
             optimizer.step()
 
-    trained_params = {
-        "conv_weight": conv_w.detach().cpu().numpy().tolist(),
-        "conv_bias": conv_b.detach().cpu().numpy().tolist(),
-        "fc_weight": fc_w.detach().cpu().numpy().tolist(),
-        "fc_bias": fc_b.detach().cpu().numpy().tolist(),
-    }
+    # ✅ Extract all weights and biases easily
+    #for name, module in model.named_modules():
+    #    print(f"Layer Name: {name}, Layer Type: {module.__class__.__name__}")
+
+    #for key, value in model.state_dict().items():
+    #    print(key)
+    #return
+    trained_params = {key: value.detach().cpu().numpy().tolist() for key, value in model.state_dict().items()}
 
     return json.dumps(trained_params)
 
+if __name__ == "__main__":
+    trained_json = train()
+    print(trained_json)
